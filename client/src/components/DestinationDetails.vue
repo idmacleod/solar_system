71c94497f235f58568e4830510663e7bed3e6adb
idmacleod <idmacleod@gmail.com>
2020-06-10 16:35:19 +0100

    Added lots of styling to DestinationDetails.vue

--- client/src/components/DestinationDetails.vue
+++ client/src/components/DestinationDetails.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="destination-details">
+    
     <h2>{{ englishName }} - {{ symbol }}</h2>
-    <table>
-      <tr><th>Mean Diameter:</th><td>{{ diameter }} km</td></tr>
-      <tr><th>Gravity:</th><td>{{ gravity }} m/s per second</td></tr>
-      <tr><th>Escape Velocity:</th><td>{{ escape }} m/s</td></tr>
-      <tr><th>Density:</th><td>{{ density }} grams per cubic cm</td></tr>
-      <tr v-if="moonCount"><th>Number Of Moons:</th><td>{{ moonCount }}</td></tr>
-      <tr v-if="discoveredBy"><th>Discovered By:</th><td>{{ discoveredBy }}</td></tr>
-      <tr v-if="discoveryDate"><th>Discovery Date:</th><td>{{ discoveryDate }}</td></tr>
-    </table>
-    <div v-if="description"><h3>Description</h3><p>{{ description }}</p></div>
-    <div v-if="mythology"><h3>Mythology</h3><p>{{ mythology }}</p></div>
-    <img :src="destination.db.photo" />
+    
+    <div id="card-top">
+      <table>
+        <tr><th>Mean Diameter</th><td>{{ diameter }} km</td></tr>
+        <tr><th>Gravity</th><td>{{ gravity }} m/s per second</td></tr>
+        <tr><th>Escape Velocity</th><td>{{ escape }} m/s</td></tr>
+        <tr><th>Density</th><td>{{ density }} grams per cubic cm</td></tr>
+        <tr v-if="moonCount"><th>Number Of Moons</th><td>{{ moonCount }}</td></tr>
+        <tr v-if="discoveredBy"><th>Discovered By</th><td>{{ discoveredBy }}</td></tr>
+        <tr v-if="discoveryDate"><th>Discovery Date</th><td>{{ discoveryDate }}</td></tr>
+      </table>
+      <img :src="destination.db.photo" />
+    </div>
+    
+    <div v-if="description" id="description"><h3>Description</h3><p>{{ description }}</p></div>
+    
+    <div v-if="mythology" id="mythology"><h3>Mythology</h3><p>{{ mythology }}</p></div>
+
   </div>
 </template>
 
@@ -59,14 +66,57 @@ export default {
 </script>
 
 <style>
-ul {
-  list-style: none;
+#destination-details {
+    border: 2px solid black;
+    margin: 10px;
+}
+
+h2 {
+  font-family: 'Nixie One';
+  margin: 0 0 0 0;
+  padding: 10px;
+  text-align: justify;
+}
+
+#card-top {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: black;
+  color: white;
+}
+
+table {
+  border: 2px solid white;
+  border-collapse: collapse;
+}
+
+th {
+  text-align: right;
+  background: white;
+  color: black;
+  padding: 5px;
+}
+
+td {
+  padding: 5px;
 }
 
 img {
-  margin: 20px;
-  max-width: 300px;
-  width: auto;
-  height: auto;
+  max-width: 200px;
+  margin: 10px;
+}
+
+#description, #mythology {
+  padding: 10px;
+}
+
+h3 {
+  margin: 0;
+}
+
+#mythology {
+  background: black;
+  color: white;
 }
 </style>

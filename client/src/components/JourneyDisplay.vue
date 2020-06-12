0e3517e2f3892f366375b8a400ee72a44733f9ce
idmacleod <idmacleod@gmail.com>
2020-06-12 11:41:24 +0100

    Adding JourneySymbols.vue component to create visual record of journey

--- client/src/components/JourneyDisplay.vue
+++ client/src/components/JourneyDisplay.vue
@@ -1,15 +1,19 @@
 <template lang="html">
   <div id="journey-display">
-    <destination-details
-      v-for="(destination, index) in journey"
-      :key="index"
-      :destination="destination"
-    />
+    <journey-symbols />
+    <div id="cards">
+      <destination-details
+        v-for="(destination, index) in journey"
+        :key="index"
+        :destination="destination"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { eventBus } from "../main.js";
+import JourneySymbols from "./JourneySymbols.vue";
 import DestinationDetails from "./DestinationDetails.vue";
 
 export default {
@@ -19,19 +23,25 @@ export default {
       journey: []
     };
   },
-  components: {
-    "destination-details": DestinationDetails,
+  computed: {
+    symbols: function () {
+      return this.journey.map(destination => destination.db.symbol);
+    }
   },
   mounted() {
     eventBus.$on("addToJourney", (destination) => {
       this.journey.push(destination);
     });
   },
+  components: {
+    "journey-symbols": JourneySymbols,
+    "destination-details": DestinationDetails
+  }
 };
 </script>
 
 <style>
-#journey-display {
+#cards {
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 20px;
---
+++ client/src/components/JourneySymbols.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="journey-symbols">
+    <h1><span class="sym">⨁</span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'journey-symbols',
+  props: ['symbols']
+}
+</script>
+
+<style>
+#journey-symbols h1 {
+  font-family: 'Nixie One';
+  background: black;
+  color: white;
+  margin: 0 0 10px 0;
+  padding: 10px;
+}
+</style>
